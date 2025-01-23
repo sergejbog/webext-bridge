9cf818e11f21af67875dea852c988a37abb4bf80
Sergej Bogatinoski <serbogatinoski@gmail.com>
2025-01-23 16:48:39 +0100

    Adding side-panel integration

--- src/internal/endpoint.ts
+++ src/internal/endpoint.ts
@@ -1,20 +1,19 @@
-import type { Endpoint, RuntimeContext } from '../types'
+import type { Endpoint, RuntimeContext } from '../types';
 
-const ENDPOINT_RE = /^((?:background$)|devtools|popup|options|content-script|window)(?:@(\d+)(?:\.(\d+))?)?$/
+const ENDPOINT_RE = /^((?:background$)|devtools|popup|options|side-panel|content-script|window)(?:@(\d+)(?:\.(\d+))?)?$/;
 
 export const parseEndpoint = (endpoint: string): Endpoint => {
-  const [, context, tabId, frameId] = endpoint.match(ENDPOINT_RE) || []
+   const [, context, tabId, frameId] = endpoint.match(ENDPOINT_RE) || [];
 
-  return {
-    context: context as RuntimeContext,
-    tabId: +tabId,
-    frameId: frameId ? +frameId : undefined,
-  }
-}
+   return {
+      context: context as RuntimeContext,
+      tabId: +tabId,
+      frameId: frameId ? +frameId : undefined,
+   };
+};
 
 export const formatEndpoint = ({ context, tabId, frameId }: Endpoint): string => {
-  if (['background', 'popup', 'options'].includes(context))
-    return context
+   if (['background', 'popup', 'options', 'side-panel'].includes(context)) return context;
 
-  return `${context}@${tabId}${frameId ? `.${frameId}` : ''}`
-}
+   return `${context}@${tabId}${frameId ? `.${frameId}` : ''}`;
+};
